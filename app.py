46593d98830c44eedcf6e79967ddd838f49ac85a
Zachary Bannor <zachary.bannor@spr.com>
2019-07-22 15:39:53 -0500

    start css, layout

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ import flask
 
 app = flask.Flask(__name__)
 
+def _get_datapoints():
+    return [
+        {"name": "data_1", "value": "1"},
+        {"name": "data_2", "value": "2"},
+        {"name": "data_3", "value": "3"},
+    ]
+
 @app.route("/")
 def index():
-    return "I'm awake"
+    data = _get_datapoints()
+    return flask.render_template("index.html", data=data)
+
+@app.route("/about")
+def about():
+    return flask.render_template("/about.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
